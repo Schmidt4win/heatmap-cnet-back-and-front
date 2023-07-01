bc380e2ab484df7732ec82bfc21d7a1a55bfa781
Guilherme Schmidt <111580564+Schmidt4win@users.noreply.github.com>
2023-07-01 09:17:05 -0300

    formating the document
    retirando comentários inúteis (Lucas Montando) no código e formatando bonitinho

--- BACK-END/src/controllers/oltController.js
+++ BACK-END/src/controllers/oltController.js
@@ -1,140 +1,145 @@
 import ramaisModel from "../models/ramaisOlt.js";
-import { Client } from 'ssh2';
-import dotenv from 'dotenv';
-dotenv.config()
+import { Client } from "ssh2";
+import dotenv from "dotenv";
+dotenv.config();
 class oltController {
-
-    static ListarRamais = (req, res) => {
-        ramaisModel.find((err, ramal)=>{
-        res.status(200).json(ramal)
-        }).sort({_id: -1})
-    }
-
-       
-    static verificarRamais = (req, res) => {
-      let searchString = req.body.Ramal;
-      console.log(searchString);
-      let regex = new RegExp(searchString, "i"); // "i" for case-insensitive search
-    
-      ramaisModel.find({ ID: { $regex: regex } }, (err, ramal) => {
-          if (err) {
-              console.error(err);
-              res.status(500).json({ message: `Back-end err: ${err}` });
-          } else {
-              if (ramal.length === 0) {
-                  res.status(200).json({ message: "No Ramal found" });
-              } else {
-                  res.status(200).json(ramal);
-              }
-          }
-      });
+  static ListarRamais = (req, res) => {
+    ramaisModel
+      .find((err, ramal) => {
+        res.status(200).json(ramal);
+      })
+      .sort({ _id: -1 });
   };
-  
-      
-      static listarOnu = (req, res) => {
-        let host =  req.body.oltIp;
-        const username = process.env.PARKS_USERNAME;
-        const password = `#${process.env.PARKS_PASSWORD}`; 
-        const conn = new Client();
-      
-        conn.on('ready', () => {
-          console.log('Connected to the OLT');
-          conn.shell((err, stream) => {
-            if (err) {
-              console.error(err);
-              res.status(500).json({ error: 'Error connecting to the OLT' });
-              return;
-            }
-      
-            let dataBuffer = '';
-            let onus = [];
-      
-            stream.on('close', () => {
-              console.log('Disconnected from the OLT');
-              //console.log('JSON Output:', onus);
+
+  static verificarRamais = (req, res) => {
+    let searchString = req.body.Ramal;
+    console.log(searchString);
+    let regex = new RegExp(searchString, "i"); // "i" for case-insensitive search
+
+    ramaisModel.find({ ID: { $regex: regex } }, (err, ramal) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ message: `Back-end err: ${err}` });
+      } else {
+        if (ramal.length === 0) {
+          res.status(200).json({ message: "No Ramal found" });
+        } else {
+          res.status(200).json(ramal);
+        }
+      }
+    });
+  };
+
+  static listarOnu = (req, res) => {
+    let host = req.body.oltIp;
+    const username = process.env.PARKS_USERNAME;
+    const password = `#${process.env.PARKS_PASSWORD}`;
+    const conn = new Client();
+
+    conn
+      .on("ready", () => {
+        console.log("Connected to the OLT");
+        conn.shell((err, stream) => {
+          if (err) {
+            console.error(err);
+            res.status(500).json({ error: "Error connecting to the OLT" });
+            return;
+          }
+
+          let dataBuffer = "";
+          let onus = [];
+
+          stream
+            .on("close", () => {
+              console.log("Disconnected from the OLT");
               res.json(onus);
               conn.end();
-            }).on('data', (data) => {
+            })
+            .on("data", (data) => {
               dataBuffer += data.toString();
-              // Check for a complete line
-              if (dataBuffer.includes('\n')) {
-                const lines = dataBuffer.split('\n');
-                dataBuffer = lines.pop(); // Store the incomplete line for future data
+              if (dataBuffer.includes("\n")) {
+                const lines = dataBuffer.split("\n");
+                dataBuffer = lines.pop();
                 for (const line of lines) {
-                  //console.log('Output: ' + line);
-                  // Parse and process the received line here
-                  if (line.includes('|') && !line.includes('Interface')) {
-                    const values = line.split('|').map(value => value.trim());
+                  if (line.includes("|") && !line.includes("Interface")) {
+                    const values = line.split("|").map((value) => value.trim());
                     const [gpon, onuMac, onuModel] = values;
-      
+
                     onus.push({
                       onuMac,
                       gpon,
-                      onuModel
+                      onuModel,
                     });
                   }
                 }
               }
             });
-      
-            // Send the commands to the OLT
-            stream.write('show gpon onu unconfigured\n');
-            stream.write('exit\n');
-          });
-        }).on('error', (err) => {
-          console.error(err);
-          res.status(500).json({ error: 'Error connecting to the OLT' });
-        }).connect({
-          host: host,
-          port: 22,
-          username: username,
-          password: password
+
+          stream.write("show gpon onu unconfigured\n");
+          stream.write("exit\n");
         });
-      }
-      
-      static liberarOnu = (req, res) => {
-        let oltIp = req.body.oltIp;
-        let oltPon = req.body.oltPon;
-        let onuVlan = req.body.onuVlan;
-        let gpon = oltPon;
-        let onuSerial = req.body.onuSerial;
-        let onuAlias = req.body.onuAlias;
-        let flowProfile = `bridge_vlan_${onuVlan}`;
-      
-        const host = oltIp; // Replace with the IP address of your OLT
-        const username = process.env.PARKS_USERNAME;
-        const password = `#${process.env.PARKS_PASSWORD}`;
-        const conn = new Client();
-      
-        conn.on('ready', () => {
-          console.log('Connected to the OLT');
-          conn.shell((err, stream) => {
-            if (err) throw err;
-            let dataBuffer = '';
-            let jsonOutput = {};
-      
-            stream.on('close', () => {
-              console.log('Disconnected from the OLT');
-              console.log('JSON Output:', JSON.stringify(jsonOutput, null, 2));
-              res.status(200).json(jsonOutput); // Send the JSON output as the response
+      })
+      .on("error", (err) => {
+        console.error(err);
+        res.status(500).json({ error: "Error connecting to the OLT" });
+      })
+      .connect({
+        host: host,
+        port: 22,
+        username: username,
+        password: password,
+      });
+  };
+
+  static liberarOnu = (req, res) => {
+    let oltIp = req.body.oltIp;
+    let oltPon = req.body.oltPon;
+    let onuVlan = req.body.onuVlan;
+    let gpon = oltPon;
+    let onuSerial = req.body.onuSerial;
+    let onuAlias = req.body.onuAlias;
+    let flowProfile = `bridge_vlan_${onuVlan}`;
+
+    const host = oltIp;
+    const username = process.env.PARKS_USERNAME;
+    const password = `#${process.env.PARKS_PASSWORD}`;
+    const conn = new Client();
+
+    conn
+      .on("ready", () => {
+        console.log("Connected to the OLT");
+        conn.shell((err, stream) => {
+          if (err) throw err;
+          let dataBuffer = "";
+          let jsonOutput = {};
+
+          stream
+            .on("close", () => {
+              console.log("Disconnected from the OLT");
+              console.log("JSON Output:", JSON.stringify(jsonOutput, null, 2));
+              res.status(200).json(jsonOutput);
               conn.end();
-            }).on('data', (data) => {
+            })
+            .on("data", (data) => {
               dataBuffer += data.toString();
               // Check for a complete line
-              if (dataBuffer.includes('\n')) {
-                const lines = dataBuffer.split('\n');
-                dataBuffer = lines.pop(); // Store the incomplete line for future data
+              if (dataBuffer.includes("\n")) {
+                const lines = dataBuffer.split("\n");
+                dataBuffer = lines.pop();
                 for (const line of lines) {
-                  console.log('Output: ' + line);
-                  // Parse and process the received line here
-                  if (line.includes(':')) {
-                    const [key, value] = line.split(':');
+                  console.log("Output: " + line);
+
+                  if (line.includes(":")) {
+                    const [key, value] = line.split(":");
                     const trimmedKey = key.trim();
                     const trimmedValue = value.trim();
-      
-                    if (trimmedKey === 'Status') {
+
+                    if (trimmedKey === "Status") {
                       jsonOutput[onuSerial] = { Status: trimmedValue };
-                    } else if (trimmedKey === 'Power Level' || trimmedKey === 'RSSI') {
+                    } else if (
+                      trimmedKey === "Power Level" ||
+                      trimmedKey === "RSSI"
+                    ) {
                       if (jsonOutput[onuSerial]) {
                         jsonOutput[onuSerial][trimmedKey] = trimmedValue;
                       }
@@ -143,29 +148,30 @@ class oltController {
                 }
               }
             });
-      
-            // Send the commands to the OLT show gpon onu
-            stream.write('configure terminal\n')
-            stream.write(`interface ${gpon}\n`)
-            stream.write(`onu ${onuSerial} alias ${onuAlias}\n`)
-            stream.write(`onu ${onuSerial} flow ${flowProfile}\n`)
-            stream.write(`onu ${onuSerial} vlan-translation-profile _${onuVlan} uni-port 1\n`)
-            stream.write(`onu ${onuSerial} ethernet-profile auto-on uni-port 1\n`)
-            stream.write('end\n');
-            stream.write('copy r s\n');
-            stream.write(`show gpon onu ${onuSerial} status\n`);
-            stream.write('exit\n');
-          });
-        }).connect({
-          host: host,
-          port: 22,
-          username: username,
-          password: password
+
+          stream.write("configure terminal\n");
+          stream.write(`interface ${gpon}\n`);
+          stream.write(`onu ${onuSerial} alias ${onuAlias}\n`);
+          stream.write(`onu ${onuSerial} flow ${flowProfile}\n`);
+          stream.write(
+            `onu ${onuSerial} vlan-translation-profile _${onuVlan} uni-port 1\n`
+          );
+          stream.write(
+            `onu ${onuSerial} ethernet-profile auto-on uni-port 1\n`
+          );
+          stream.write("end\n");
+          stream.write("copy r s\n");
+          stream.write(`show gpon onu ${onuSerial} status\n`);
+          stream.write("exit\n");
         });
-      };
-      
-         
-    
-    }
-    
-    export default oltController;
+      })
+      .connect({
+        host: host,
+        port: 22,
+        username: username,
+        password: password,
+      });
+  };
+}
+
+export default oltController;
